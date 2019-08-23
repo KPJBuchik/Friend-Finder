b47f4cfe945101d53fb89713bb08bf5897fea612
Kevin Buchik <kbuchik@gmail.com>
2019-08-22 18:57:43 -0700

    deployed

--- data/friends.js
+++ data/friends.js
@@ -2,8 +2,8 @@
 
 var friends = [
 {
-    name: "ron",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    name: "jeff",
+    photo: "https://m.media-amazon.com/images/M/MV5BMjIyNTM4ODk5N15BMl5BanBnXkFtZTcwMTIzMDYxMw@@._V1_.jpg",
     scores: [
         5,
         1,
@@ -18,8 +18,8 @@ var friends = [
     ],
 },
 {
-    name: "jim",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    name: "carrie",
+    photo: "https://pixel.nymag.com/imgs/daily/vulture/2015/03/16/16-carrie-brownstein.w330.h330.jpg",
     scores: [
         5,
         1,
@@ -33,8 +33,72 @@ var friends = [
         1]
 },
 {
-    name: "bill",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    name: "paul",
+    photo: "https://img.discogs.com/Iqep5VIKfQ0h5Waw_gNeVo9gTpk=/fit-in/540x567/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/A-236256-1148984408.jpeg.jpg",
+    scores: [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    ],
+},
+{
+    name: "shirley",
+    photo: "https://www.looktothestars.org/photo/5165-shirley-manson/teaser-1503333649.jpg",
+    scores: [
+        1,
+        1,
+        4,
+        1,
+        1,
+        4,
+        1,
+        5,
+        5,
+        5
+    ],
+},
+{
+    name: "john",
+    photo: "http://img6.bdbphotos.com/images/orig/n/m/nm7s58q2q4p52q4s.jpg?skj2io4l",
+    scores: [
+        1,
+        1,
+        2,
+        1,
+        4,
+        1,
+        3,
+        1,
+        2,
+        1
+    ],
+},
+{
+    name: "bob",
+    photo: "https://pbs.twimg.com/profile_images/2707576221/157d890f8394369a47c9f96e61c1d7b6_400x400.jpeg",
+    scores: [
+        1,
+        2,
+        1,
+        1,
+        4,
+        1,
+        1,
+        5,
+        1,
+        1
+    ],
+},
+{
+    name: "Michael",
+    photo: "https://www.thatericalper.com/wp-content/uploads/2015/08/remeh_1265287171_crop_550x450.jpg",
     scores: [
         1,
         1,
