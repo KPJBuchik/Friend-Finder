fea612298247c18e5b9b8208b59c2b3df2846c5c
Kevin Buchik <kbuchik@gmail.com>
2019-08-23 15:12:59 -0700

    broken logic

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -9,37 +9,47 @@ module.exports = function (app) {
 
 
     app.post("/api/friends", function (req, res) {
+        let sum1;
 
-        
-        let userScore=Infinity
+        var bestMatch = {
+            totalDifference: Infinity,
+            name: "",
+            photo: ""
+        }
+        console.log(req.body.scores)
+        for (var k = 0; k < req.body.scores.length; k++) {
+            sum1 += parseInt(req.body.scores[k])
+            console.log("sum1"+sum1)
+
+        }
         //loop over friends data 
         for (var i = 0; i < friends.length; i++) {
-            let diff=0;
-            
+            let diff = 0;
+            let currentFriend = friends[i]
             //loop over scores
-            for (var j = 0; j < req.body.scores.length; j++) {
+            for (var j = 0; j < currentFriend.scores.length; j++) {
                 let sum;
-               sum =  (req.body.scores[j])
-            
+                sum += parseInt(currentFriend.scores[j])
+
+                // console.log(sum)
                 //difference instead of sum
-                diff += parseFloat(friends[i].scores[j]) - sum
-                
-             //   sum += (friends[i].scores[j])
+                diff = Math.abs(sum1 - sum)
+                if (diff <= bestMatch.totalDifference) {
+                    // userScore=diff
+                    totalDifference = diff
+
+                    bestMatch.name = friends.name
+                    bestMatch.photo = friends.name
+                    totalDifference = diff
+                }
+
             }
-            if (diff < userScore) {
-                 userScore=diff
-				 name = friends[i].name;
-                 photo = friends[i].photo;
-            }
-            
-        
-//just grabs last entry
+            //just grabs last entry
         }
         friends.push(req.body)
-        res.json({name: name, photo: photo});
-        console.log(name)
-        console.log(req.body.scores)
+        res.json(bestMatch);
+        // console.log(req.body.scores)
+        // console.log(bestMatch)
     })
 }
 
-// loop over user scores to get a sum or friends scores length?
